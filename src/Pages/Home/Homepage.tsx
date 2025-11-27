8574c57b74f6c15b7a6915b78de36171133f027a
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-11-27 15:09:44 +0600

    feat: add hero section

--- src/Pages/Home/Homepage.tsx
+++ src/Pages/Home/Homepage.tsx
@@ -1,4 +1,5 @@
-import { Hero } from "@/components/Hero";
+import { Hero } from "@/components/modules/HomePage/HeroSection/Hero";
+
 
 
 export default function HomePage() {
---
+++ src/components/modules/HomePage/HeroSection/Hero.tsx
@@ -0,0 +1,61 @@
+// import { Button } from "./ui/button";
+// import { buttonVariants } from "./ui/button";
+// import { HeroCards } from "./HeroCards";
+// import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Github } from "lucide-react";
+import { HeroCards } from "./HeroCards";
+
+export const Hero = () => {
+  return (
+    <section className="container grid lg:grid-cols-2 place-items-center py-6 gap-10 ">
+      <div className="text-center lg:text-start space-y-6">
+        <main className="text-5xl md:text-6xl font-bold">
+          <h1 className="inline">
+            <span className="inline bg-gradient-to-r from-[#ff6900]  to-[oklch(0.47_0.157_37.304)] text-transparent bg-clip-text">
+            VaultPay
+            </span>{" "}
+            digital wallet
+          </h1>{" "}
+          for{" "}
+          <h2 className="inline">
+            <span className="inline bg-gradient-to-r to-[oklch(0.47_0.157_37.304)]  via-[oklch(0.553_0.195_38.402)] from-[oklch(0.646_0.222_41.116)] text-transparent bg-clip-text">
+            secure 
+            </span>{" "}
+            transactions
+          </h2>
+        </main>
+
+        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+          Build your React landing page effortlessly with the required sections
+          to your project.
+        </p>
+
+        <div className="space-y-4 md:space-y-0 md:space-x-4">
+          <Button className="w-full md:w-1/3">Get Started</Button>
+
+          <a
+            rel="noreferrer noopener"
+            href="https://github.com/leoMirandaa/shadcn-landing-page.git"
+            target="_blank"
+            className={`w-full md:w-1/3 ${buttonVariants({
+              variant: "outline",
+            })}`}
+          >
+            Github Repository
+            <Github className="ml-2 w-5 h-5" />
+          </a>
+        </div>
+      </div>
+
+      {/* Hero cards sections */}
+      <div className="z-10">
+        <HeroCards />
+      </div>
+
+      {/* Shadow effect */}
+      <div className="shadow"></div>
+    </section>
+  );
+};
